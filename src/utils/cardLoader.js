f2886a5a52dceb45bc2307f4bdae47d28f379b80
RAZAFIMANDIMBY Zo Hery Niaina Jacky <zorazafi@CPPSYSTEM.LOCAL>
2026-05-10 16:13:57 +0300

    update file

--- src/utils/cardLoader.js
+++ src/utils/cardLoader.js
@@ -1,10 +1,9 @@
-const cardImages = import.meta.glob('/src/image/card2/*.svg', { eager: true, query: '?url', import: 'default' });
+const cardImages = import.meta.glob('/src/image/card2/*.svg', { eager: true, import: 'default' });
 
 export const getCardImage = (cardId) => {
-    // Vite needs the path to start with /src/
     const finalId = cardId.replace('T', '0').toUpperCase();
     const path = `/src/image/card2/${finalId}.svg`;
     
-    // Accéder au résultat du glob
+    // Le glob eager donne directement l'URL traitée par Vite
     return cardImages[path];
 };
