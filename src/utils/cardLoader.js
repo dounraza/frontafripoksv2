0631cbe1fb00c7990362dbca32db4464e2616ba9
RAZAFIMANDIMBY Zo Hery Niaina Jacky <zorazafi@CPPSYSTEM.LOCAL>
2026-05-10 16:38:29 +0300

    cardloader

--- src/utils/cardLoader.js
+++ src/utils/cardLoader.js
@@ -1,18 +1,7 @@
-const cards = import.meta.glob("/src/image/card2/*.svg", {
-  eager: true,
-  import: "default",
-});
+const cardImages = import.meta.glob('/src/image/card2/*.svg', { eager: true });
 
 export const getCardImage = (cardId) => {
-  const finalId = cardId.replace('T', '0').toUpperCase();
-  const path = `/src/image/card2/${finalId}.svg`;
-  
-  const src = cards[path];
-  
-  if (!src) {
-    console.warn(`Carte non trouvée : ${path}`);
-    return null;
-  }
-  
-  return src;
+    const finalId = cardId.replace('T', '0').toUpperCase();
+    const path = `/src/image/card2/${finalId}.svg`;
+    return cardImages[path]?.default || cardImages[path];
 };
